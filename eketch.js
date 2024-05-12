6cebe7d5278345f8b3944bd1cb11a905f94b1d2c
MarcosBarbosa03servidor <marcos.barbosa03@educacao.sp.gov.br>
2024-05-12 16:27:40 -0300

    Create eketch.js

---
+++ eketch.js
@@ -0,0 +1,33 @@
+olinha = 300;
+let yBolinha = 200;
+let diametro = 15;
+
+let raio = diametro / 2;
+
+let velocidadeXBolinha = 6;
+let velocidadeYBolinha = 6;
+
+//variáveis da raquete
+let xRaquete = 5;
+let yRaquete = 150;
+let raqueteComprimento = 10;
+let raqueteAltura = 90;
+
+//variáveis do oponente
+let xRaqueteOponente = 585;
+let yRaqueteOponente = 150;
+let velocidadeYOponente;
+
+let colidiu = false;
+
+
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+    background(0);
+    mostraBolinha();
+    movimentaBolinha();
+    verificaColisaoBorda();
+    mostraRaquete(xRaquete, yRaquete);
